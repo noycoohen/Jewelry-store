df794d4eaa0fdfe7080ec3ac77c4ae7d988560fa
noycoohen <114675124+noycoohen@users.noreply.github.com>
2024-02-16 16:17:04 +0200

    updated initdataBase

--- server/DB/utils/initDataBase.ts
+++ server/DB/utils/initDataBase.ts
@@ -7,115 +7,29 @@ export const initDatabase = async () => {
   try {
     const usersData = [
       {
-        name: {
-          first: "admin",
-          middle: "",
-          last: "user",
-        },
-        phone: "0527345882",
-        email: "Admin@example.com",
+        name: "admin",
+        email: "admin@example.com",
         password: "Ab1234Ab!",
-        image: {
-          url: "https://example.com/images/johndoe.jpg",
-          alt: "Profile picture of John Doe",
-        },
-        address: {
-          city: "New York",
-          country: "USA",
-          houseNumber: 123,
-          state: "NY",
-          street: "Main St",
-          zip: 34555,
-        },
-        isBusiness: true,
         isAdmin: true,
       },
       {
-        name: {
-          first: "Business",
-          middle: "",
-          last: "user",
-        },
-        phone: "0527345883",
-        email: "Business@example.com",
+        name: "noy",
+        email: "Regularr@example.com",
         password: "Ab1234Ab!",
-        image: {
-          url: "https://example.com/images/johndoe.jpg",
-          alt: "Profile picture of John Doe",
-        },
-        address: {
-          city: "New York",
-          country: "USA",
-          houseNumber: 123,
-          state: "NY",
-          street: "Main St",
-          zip: 34555,
-        },
-        isBusiness: true,
       },
       {
-        name: {
-          first: "Regular",
-          middle: "",
-          last: "user",
-        },
-        phone: "0527345884",
+        name: "noy",
         email: "Regular@example.com",
         password: "Ab1234Ab!",
-        image: {
-          url: "https://example.com/images/johndoe.jpg",
-          alt: "Profile picture of John Doe",
-        },
-        address: {
-          city: "New York",
-          country: "USA",
-          houseNumber: 123,
-          state: "NY",
-          street: "Main St",
-          zip: 34555,
-        },
-        isBusiness: false,
       },
     ];
 
     const cardData = [
       {
-        title: "First Card",
-        subtitle: "Software Engineer",
+        title: "Third Card",
         description:
           "Experienced software engineer with a passion for developing innovative programs that expedite the efficiency and effectiveness of organizational success.",
-        phone: "050-1234567",
-        email: "FirstCard@example.com",
-        web: "http://www.johndoe.com",
-        address: {
-          street: "123 Main St",
-          city: "San Francisco",
-          state: "California",
-          zip: "94101",
-          country: "USA",
-          houseNumber: "123",
-        },
-        image: {
-          url: "https://picsum.photos/200",
-          alt: "John Doe",
-        },
-      },
-      {
-        title: "Second Card",
-        subtitle: "Software Engineer",
-        description:
-          "Experienced software engineer with a passion for developing innovative programs that expedite the efficiency and effectiveness of organizational success.",
-        phone: "050-1234567",
-        email: "SecondCard@example.com",
-        web: "http://www.johndoe.com",
-        address: {
-          street: "123 Main St",
-          city: "San Francisco",
-          state: "California",
-          zip: "94101",
-          country: "USA",
-          houseNumber: "123",
-        },
+        price: 30,
         image: {
           url: "https://picsum.photos/200",
           alt: "John Doe",
@@ -123,20 +37,19 @@ export const initDatabase = async () => {
       },
       {
         title: "Third Card",
-        subtitle: "Software Engineer",
         description:
           "Experienced software engineer with a passion for developing innovative programs that expedite the efficiency and effectiveness of organizational success.",
-        phone: "050-1234567",
-        email: "ThirdCard@example.com",
-        web: "http://www.johndoe.com",
-        address: {
-          street: "123 Main St",
-          city: "San Francisco",
-          state: "California",
-          zip: "94101",
-          country: "USA",
-          houseNumber: "123",
+        price: 30,
+        image: {
+          url: "https://picsum.photos/200",
+          alt: "John Doe",
         },
+      },
+      {
+        title: "Third Card",
+        description:
+          "Experienced software engineer with a passion for developing innovative programs that expedite the efficiency and effectiveness of organizational success.",
+        price: 30,
         image: {
           url: "https://picsum.photos/200",
           alt: "John Doe",
@@ -163,12 +76,14 @@ export const initDatabase = async () => {
 
       // Check if cards for the user with userId exist
       const cardsCount = await Card.countDocuments({ user_id: userId });
+
       if (cardsCount === 0) {
         // Cards for the user with userId not found, create and save to the database
         const cardsWithUserId = cardData.map((card) => ({
           ...card,
           user_id: userId,
         }));
+
         await Card.insertMany(cardsWithUserId);
         console.log(
           chalk.greenBright(
