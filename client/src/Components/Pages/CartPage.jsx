b2243b7eff90f575c4227f5b34ac4ec7ac5e295c
noycoohen <114675124+noycoohen@users.noreply.github.com>
2024-02-18 20:07:52 +0200

    Save the cart with localStorage

--- client/src/Components/Pages/CartPage.jsx
+++ client/src/Components/Pages/CartPage.jsx
@@ -1,10 +1,18 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { CartProductsContext } from "../../App";
 import "../../Services/style.css";
 
 export const CartPage = () => {
   const productData = useContext(CartProductsContext);
-  const [products, setProducts] = useState(productData);
+  const [products, setProducts] = useState(() => {
+    const savedProducts = localStorage.getItem("Cart");
+    return savedProducts ? JSON.parse(savedProducts) : productData;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("Cart", JSON.stringify(products));
+  }, [products]);
+
   const removeProduct = (id) => {
     setProducts(products.filter((product) => product.id !== id));
   };
@@ -12,27 +20,32 @@ export const CartPage = () => {
     <div>
       <h1>Shopping Cart</h1>
       <div className="cart-container">
-        {products.map((product) => (
-          <div key={product.id} className="product-item">
-            <img
-              src={product.imageId}
-              alt={product.name}
-              className="product-image"
-            />
-            <div className="product-info">
-              <div className="product-name">{product.name}</div>
-              <div className="product-description">{product.description}</div>
-              <p>{product.price}</p>
+        {products.length > 0 ? (
+          products.map((product) => (
+            <div key={product.id} className="product-item">
+              <img
+                src={product.imageId}
+                alt={product.name}
+                className="product-image"
+              />
+              <div className="product-info">
+                <div className="product-name">{product.name}</div>
+                <div className="product-description">{product.description}</div>
+                <p>{product.price}</p>
+              </div>
+              <button
+                onClick={() => removeProduct(product.id)}
+                style={{ marginTop: "10px" }}
+              >
+                Remove
+              </button>
             </div>
-            <button
-              onClick={() => removeProduct(product.id)}
-              style={{ marginTop: "10px" }}
-            >
-              Remove
-            </button>
-          </div>
-        ))}
+          ))
+        ) : (
+          <div className="emptyCart-message">Cart is empty</div>
+        )}
       </div>
+      <p>Total Price:</p>
     </div>
   );
 };
