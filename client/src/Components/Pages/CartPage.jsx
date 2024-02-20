068159dd713e014115b42944b7f7076e071756be
noycoohen <114675124+noycoohen@users.noreply.github.com>
2024-02-20 21:35:07 +0200

    add container to cart page

--- client/src/Components/Pages/CartPage.jsx
+++ client/src/Components/Pages/CartPage.jsx
@@ -1,40 +1,48 @@
 import React, { useContext } from "react";
 import "../../Services/style.css";
 import { CartContext } from "../../Contexts/CartProvider";
+import { Container } from "@mui/material";
 
 export const CartPage = () => {
   const { cart, removeFromCart } = useContext(CartContext);
 
   return (
-    <div>
-      <h1>Shopping Cart</h1>
-      <div className="cart-container">
-        {cart.length > 0 ? (
-          cart.map((product) => (
-            <div key={product.id} className="product-item">
-              <img
-                src={product.image?.url}
-                alt={product.name}
-                className="product-image"
-              />
-              <div className="product-info">
-                <div className="product-name">{product.title}</div>
-                <div className="product-description">{product.description}</div>
-                <p>{product.price}</p>
+    <Container
+      maxWidth="lg"
+      sx={{ textAlign: "center", mt: "3rem", minHeight: "700px" }}
+    >
+      <div>
+        <h1>Shopping Cart</h1>
+        <div className="cart-container">
+          {cart.length > 0 ? (
+            cart.map((product) => (
+              <div key={product.id} className="product-item">
+                <img
+                  src={product.image?.url}
+                  alt={product.name}
+                  className="product-image"
+                />
+                <div className="product-info">
+                  <div className="product-name">{product.title}</div>
+                  <div className="product-description">
+                    {product.description}
+                  </div>
+                  <p>{product.price}</p>
+                </div>
+                <button
+                  onClick={() => removeFromCart(product._id)}
+                  style={{ marginTop: "10px" }}
+                >
+                  Remove
+                </button>
               </div>
-              <button
-                onClick={() => removeFromCart(product._id)}
-                style={{ marginTop: "10px" }}
-              >
-                Remove
-              </button>
-            </div>
-          ))
-        ) : (
-          <div className="emptyCart-message">Cart is empty</div>
-        )}
+            ))
+          ) : (
+            <div className="emptyCart-message">Cart is empty</div>
+          )}
+        </div>
+        <p>Total Price:</p>
       </div>
-      <p>Total Price:</p>
-    </div>
+    </Container>
   );
 };
